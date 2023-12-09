b1f469b904860ab0b39d6dc039537f47309322ac
Ljubica <markoljubicic77@gmail.com>
2023-12-09 04:08:53 +0100

    changed the manual input of data to reading it from the data.json file using fetch api and added a working version of the edit function

--- scripts/app.js
+++ scripts/app.js
@@ -1,81 +1,20 @@
-const data = {
-  currentUser: {
-    image: {
-      png: "./images/avatars/image-juliusomo.png",
-      webp: "./images/avatars/image-juliusomo.webp",
-    },
-    username: "juliusomo",
-  },
-  comments: [
-    {
-      parent: 0,
-      id: 1,
-      content:
-        "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-      createdAt: "1 month ago",
-      score: 12,
-      user: {
-        image: {
-          png: "./images/avatars/image-amyrobson.png",
-          webp: "./images/avatars/image-amyrobson.webp",
-        },
-        username: "amyrobson",
-      },
-      replies: [],
-    },
-    {
-      parent: 0,
-      id: 2,
-      content:
-        "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-      createdAt: "2 weeks ago",
-      score: 5,
-      user: {
-        image: {
-          png: "./images/avatars/image-maxblagun.png",
-          webp: "./images/avatars/image-maxblagun.webp",
-        },
-        username: "maxblagun",
-      },
-      replies: [
-        {
-          parent: 2,
-          id: 1,
-          content:
-            "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-          createdAt: "1 week ago",
-          score: 4,
-          replyingTo: "maxblagun",
-          user: {
-            image: {
-              png: "./images/avatars/image-ramsesmiron.png",
-              webp: "./images/avatars/image-ramsesmiron.webp",
-            },
-            username: "ramsesmiron",
-          },
-        },
-        {
-          parent: 2,
-          id: 1,
-          content:
-            "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-          createdAt: "2 days ago",
-          score: 2,
-          replyingTo: "ramsesmiron",
-          user: {
-            image: {
-              png: "./images/avatars/image-juliusomo.png",
-              webp: "./images/avatars/image-juliusomo.webp",
-            },
-            username: "juliusomo",
-          },
-        },
-      ],
-    },
-  ],
+const fetchData = async () => {
+  try {
+    const response = await fetch("../data.json");
+    const jsonData = await response.json();
+    return jsonData;
+  } catch (error) {
+    console.log(error);
+  }
 };
+var data;
+const main = async () => {
+  data = await fetchData();
+  console.log(data);
+  initComments();
+};
+main();
 
-console.log(data);
 const appendFrag = (frag, parent) => {
     var children = [].slice.call(frag.childNodes, 0);
     parent.appendChild(frag);
@@ -139,8 +78,23 @@ const createCommentNode = (commentObject) => {
     commentNode.querySelector(".del").addEventListener("click", () => {
       promptDel(commentObject);
     });
+    commentNode.querySelector(".edit").addEventListener("click", (e) => {
+      const path = e.composedPath()[3].querySelector(".c_body");
+
+      if (
+        path.getAttribute("contenteditable") == false ||
+        path.getAttribute("contenteditable") == null
+      ) {
+        path.setAttribute("contenteditable", true);
+        path.focus()
+      } else {
+        path.removeAttribute("contenteditable");
+      }
+      
+    });
+    return commentNode;
   }
-  //edit?
+  
   return commentNode;
 }
 
@@ -171,5 +125,3 @@ function initComments(
         appendComment(parent, commentNode, parentId);
     });
 }
-
-initComments();
